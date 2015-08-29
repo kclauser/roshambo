4f466a91750385c5f10cf02c2fad16348155fc9a
Kurtis Clauser <Kurtis@Kurtiss-MacBook-Pro.local>
2015-08-29 17:05:10 -0400

    Adventure mode working fully, but unpolished

--- roshambo.rb
+++ roshambo.rb
@@ -11,20 +11,23 @@
 # else
 #   puts "no player"
 # end #==========================================================================
-player_name = nil
-player_choice = nil
-choices = %w(rock paper scissors)
+@player_name = nil
+@player_choice = nil
+@draws = @wins = @losses = 0
+@bout_win = 0
+@bout_loss = 0
+Choices = %w(rock paper scissors)
 
 puts "Welcome to the Roshambo simulator, please enter your name to initialize the simulation."
-player_name = gets.chomp.capitalize
+@player_name = gets.chomp.capitalize
 
 
-if player_name.empty?
+if @player_name.empty?
   puts "Player not detected, exiting simulation."
   exit
 else
   puts "========================================"
-  puts "Player detected, welcome #{player_name}"
+  puts "Player detected, welcome #{@player_name}"
   puts "========================================"
   puts "..."
   puts "========================================"
@@ -46,25 +49,44 @@ else
   puts "..."
 end
 
-#PUT SOME KIND OF LOOP HERE
-begin
-  puts "#{player_name}, please type your choice, rock, paper, or scissors."
-  player_choice = gets.chomp.downcase
-end until choices.include?(player_choice)
+def game
+  begin
+    puts "#{@player_name}, please type your choice, rock, paper, or scissors."
+    @player_choice = gets.chomp.downcase
+  end until Choices.include?(@player_choice)
 
-computer_choice = rand(3)
+  computer_choice = rand(3)
 
-puts "You chose #{player_choice}, computer choose #{choices[computer_choice]}"
+  puts "You chose #{@player_choice}, computer choose #{Choices[computer_choice]}"
 
-if player_choice == choices[computer_choice]
-  puts "Draw."
-elsif player_choice == choices[computer_choice - 1]
-  puts "Computer wins"
-else
-  puts "player wins"
+  if @player_choice == Choices[computer_choice]
+    puts "Draw."
+    @draws  += 1
+    puts "Wins: #{@wins}, Losses: #{@losses}, draws: #{@draws}"
+  elsif @player_choice == Choices[computer_choice - 1]
+    puts "Computer wins"
+    @losses += 1
+    puts "Wins: #{@wins}, Losses: #{@losses}, draws: #{@draws}"
+  else
+    puts "player wins"
+    @wins += 1
+    puts "Wins: #{@wins}, Losses: #{@losses}, draws: #{@draws}"
+  end
+
+  if @wins == 2
+    puts "Bout won!"
+    @bout_win += 1
+    @wins = @losses = 0
+  elsif @losses == 2
+    puts "Bout lost!"
+    @bout_loss += 1
+    @wins = @losses = 0
+  end
 end
 
+game until @bout_win == 2 || @bout_loss == 2
+
 puts "this is the player choice"
-puts player_choice
+puts @player_choice
 puts "this is the ai choice"
 puts computer_choice
